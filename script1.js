da7f6c3415e9b271ba622f8203cd47c7b9d2d8d3
shravani <shravanikaware@gmail.com>
2022-01-31 23:14:28 +0530

    initial commit

---
+++ script1.js
@@ -0,0 +1,16 @@
+console.log("running...")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('.sidebargo')){
+        document.querySelector('.bar').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }
+    else{
+        document.querySelector('.bar').style.display='none'
+        setTimeout(() => {
+            
+            document.querySelector('.cross').style.display='inline'
+        },350);
+    }
+})
